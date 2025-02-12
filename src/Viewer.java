aede3ec330d149fed5dc3401f32a4ac1a024c705
macos <macos@MacBook-Air-65.local>
2025-02-12 16:58:49 +0000

    UPDATE

--- src/Viewer.java
+++ src/Viewer.java
@@ -160,7 +160,6 @@ public class Viewer {
         x = x + width + offset;
         y = startY;
 
-        System.out.println();
 
         JButton buttonPercent = new JButton("%");
         buttonPercent.setBounds(x,y,width,height);
